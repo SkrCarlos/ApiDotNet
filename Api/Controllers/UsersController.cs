a85f9ef8f4e45865ca1937a1682350d569ca5055
Carlos Carreon <skrcarlos_carreon@outlook.com>
2023-08-29 08:02:10 -0600

    Avance 29 de Agosto 2023

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]  // api/users
+public class UsersController : ControllerBase 
+{
+    private readonly DataContext _context;
+    public UsersController( DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+        return users;
+    }
+}
